38250625336b6a11b03dac7fc85f9d026fa48d85
Terrence Tejada <tejada.terrence@gmail.com>
2018-07-25 12:09:06 +0800

    Fix login page layout theme

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,16 @@ Route::post('/storeQueueEntries/{driverqueue_id}','QueueEntriesController@storeQ
 Route::get('/getQueueEntries/{driverqueue_id}','QueueEntriesController@getQueueEntries');
 Route::get('/queueEntry/{driverqueue}','QueueEntriesController@queueEntry');
 
-Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 /**
 *  Authentication Route
--- webpack.mix.js
+++ webpack.mix.js
@@ -47,4 +47,4 @@ mix.styles([
         // 'resources/assets/js/dataTables.bootstrap4.js',
         // 'resources/assets/js/sb-admin.js',
     ], 'public/js/all.js')
-    .browserSync('http://localhost/driver_rfid/public/inspects/deactivate/1707');
+    .browserSync('http://localhost/driver_rfid/public/login');
