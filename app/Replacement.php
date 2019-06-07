cfc737e0125fba2b2d342a661725041f3ddeeeeb
Terrence Tejada <tejada.terrence@gmail.com>
2019-06-07 16:12:10 +0800

    Add Softdelete to replacements model

--- app/Replacement.php
+++ app/Replacement.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Replacement extends Model
 {
+
+    use SoftDeletes;
+
     protected $guarded = [
         'id',
     ];
